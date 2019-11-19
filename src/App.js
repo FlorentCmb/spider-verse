e817010a90fb3874befae5e595f7aada43f0dd51
FlorentCmb <florent.cmb77@gmail.com>
2019-11-19 17:45:37 +0100

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // Import librairies
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+// Import components & screens
+import Home from './screens/Home'
 
 // Import styles
 import './App.css'
@@ -8,7 +12,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        App
+        <Switch>
+
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          
+        </Switch>
       </div>
     )
   }
