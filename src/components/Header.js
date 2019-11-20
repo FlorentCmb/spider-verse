33dd5f7f0b376376f643ef038fb0128d78c38b3d
FlorentCmb <florent.cmb77@gmail.com>
2019-11-20 09:35:04 +0100

    Add a ttf font

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// Import styles
+import './Header.css'
+
 export default class Header extends React.Component {
 
     render() {
 
         return(
-            <div>
+            <div className="Header">
                 <Link to='/'>Home</Link>
             </div>
         )
